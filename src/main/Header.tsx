dd224913662c75a59a3d66f3a65a95b315aed95c
vgmda <vasilegancin@gmail.com>
2022-09-18 23:57:40 +0100

    Fix and run the program

--- src/main/Header.tsx
+++ src/main/Header.tsx
@@ -1,15 +1,21 @@
+import React from 'react';
 import logo from "./GloboLogo.png";
 
 
-const Header = () => {
-    return (
-        <header className="row mb-4">
-            <div className="col-5">
-                <img src={logo} className="logo" alt="logo"/>
-            </div>
-            <div className="col-7 mt-5 subtitle"></div>
-        </header>
-    )
-}
+type Args = {
+  subtitle: string;
+};
+
+const Header = ({ subtitle }: Args) => {
+  
+  return (
+    <header className="row mb-4">
+      <div className="col-5">
+        <img src={logo} className="logo" alt="logo"/>
+      </div>
+      <div className="col-7 mt-5 subtitle">{subtitle}</div>
+    </header>
+  );
+};
 
 export default Header;
