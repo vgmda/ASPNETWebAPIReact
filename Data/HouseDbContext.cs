4b14edb91214899cd5cd6a5c05675d41e2367f06
vgmda <vasilegancin@gmail.com>
2022-09-19 20:33:53 +0100

    Adding DB support

---
+++ Data/HouseDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+public class HouseDbContext : DbContext
+{
+    public DbSet<HouseEntity> House => Set<HouseEntity>();
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        optionsBuilder.UseSqlite($"Data Source={Path.Join(path, "housesprod.db")}");
+
+    }
+
+}
