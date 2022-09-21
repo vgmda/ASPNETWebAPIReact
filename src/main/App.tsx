890bbf48dc8d76ca571f90e5b0fcf93c25533667
vgmda <vasilegancin@gmail.com>
2022-09-21 23:16:03 +0100

    Fetch the houses from API

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import HouseList from '../house/HouseList';
 
 function App() {
   return (
     <div className="container">
-      <Header subtitle="Providing houses all over the world"/>
+      <Header subtitle="Providing houses all over the world" />
+      <HouseList/>
     </div>
   );
 }
