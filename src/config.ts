fe6c47a4b348cf374d29af0b0610f2a2afe50cdc
vgmda <vasilegancin@gmail.com>
2022-09-22 23:06:38 +0100

    Adding currency formatter

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,13 @@
 const config = {
     baseApiUrl: "https://localhost:4000",
-  };
+};
   
+const currencyFormatter = Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "EUR",
+  maximumFractionDigits: 0
+});
 
-  export default config;
+export default config;
+export {currencyFormatter}
   
--- src/house/HouseList.tsx
+++ src/house/HouseList.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import useFetchHouses from '../hooks/HouseHooks';
+import { currencyFormatter } from '../config';
+import ApiStatus from '../ApiStatus';
 
 
 const HouseList = () => {
-  const {data} = useFetchHouses();
+  const { data, status, isSuccess } = useFetchHouses();
+  
+  if (!isSuccess)
+    return <ApiStatus status={status} />
 
     return (
         <div>
@@ -25,7 +30,7 @@ const HouseList = () => {
                   <tr key={h.id}>
                     <td>{h.address}</td>
                     <td>{h.country}</td>
-                    <td>{h.price}</td>
+                    <td>{currencyFormatter.format(h.price)}</td>
                   </tr>
                 ))}
             </tbody>
