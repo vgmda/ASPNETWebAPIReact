c488adfeb8f0997c71a7afe01f5adcfc098012c7
vgmda <vasilegancin@gmail.com>
2022-09-23 20:30:16 +0100

    Added HouseDetailDto

--- Data/HouseRepository.cs
+++ Data/HouseRepository.cs
@@ -17,7 +17,17 @@ public class HouseRepository : IHouseRepository
 
     public async Task<List<HouseDto>> GetAll()
     {
-        return await context.Houses.Select(h => new HouseDto(h.Id, h.Address, h.Country, h.Price))
-        .ToListAsync();
+        return await context.Houses.Select(h => new HouseDto(h.Id, h.Address, h.Country, h.Price)).ToListAsync();
+    }
+
+    public async Task<HouseDetailDto?> Get(int id)
+    {
+        var e = await context.Houses.SingleOrDefaultAsync(h => h.Id == id);
+        if (e == null)
+        {
+            return null;
+        }
+
+        return new HouseDetailDto(e.Id, e.Address, e.Country, e.Price, e.Description, e.Photo);
     }
 }
---
+++ Dtos/HouseDetailDto.cs
@@ -0,0 +1,2 @@
+public record HouseDetailDto(int Id, string? Address, string? Country, int Price,
+    string? Description, string? Photo);
