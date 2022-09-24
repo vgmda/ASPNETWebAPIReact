93f5ae93fd7f24e0e206880753cc71f6404b63e3
vgmda <vasilegancin@gmail.com>
2022-09-25 00:43:51 +0100

    Implement HouseForm.tsx

--- src/house/HouseForm.tsx
+++ src/house/HouseForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useFetchHouses from '../hooks/HouseHooks';
 import { currencyFormatter } from '../config';
 import ApiStatus from '../ApiStatus';
@@ -10,7 +10,66 @@ type Args = {
     submitted: (house: House) => void;
 }
 
-const HouseForm = () => {
+const HouseForm = ({ house, submitted }: Args) => {
+    // Spread operator (...) allows us to quickly copy all or part of an existing array or object into another array or object.
+    const [houseState, setHouseState] = useState({ ...house });
+
+    const onSubmit: React.MouseEventHandler<HTMLButtonElement> = async (e) => {
+        e.preventDefault();
+        submitted(houseState);
+    }
+
+    return (
+        <form className="mt-2">
+          <div className="form-group">
+            <label htmlFor="address">Address</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Address"
+              value={houseState.address}
+              onChange={(e) =>
+                setHouseState({ ...houseState, address: e.target.value })
+              }/>
+          </div>
+          <div className="form-group mt-2">
+            <label htmlFor="country">Country</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Country"
+              value={houseState.country}
+              onChange={(e) =>
+                setHouseState({ ...houseState, country: e.target.value })
+              }/>
+          </div>
+          <div className="form-group mt-2">
+            <label htmlFor="description">Description</label>
+            <textarea
+              className="form-control"
+              placeholder="Description"
+              value={houseState.description}
+              onChange={(e) =>
+                setHouseState({ ...houseState, description: e.target.value })
+              }/>
+          </div>
+          <div className="form-group mt-2">
+            <label htmlFor="price">Price</label>
+            <input
+              type="number"
+              className="form-control"
+              placeholder="Price"
+              value={houseState.price}
+              onChange={(e) =>
+                setHouseState({ ...houseState, price: parseInt(e.target.value) })
+              }/>
+          </div>
+          <button
+            className="btn btn-primary mt-2"
+            disabled={!houseState.address || !houseState.country}
+                onClick={onSubmit}>Submit</button>
+        </form>
+      );
 
 }
 
