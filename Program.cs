bb92a4bcfeebf6206402d3e913b883cd2fe67724
vgmda <vasilegancin@gmail.com>
2022-09-24 23:12:57 +0100

    Added the 3 end-points (Create, Update, Delete)

--- Program.cs
+++ Program.cs
@@ -38,6 +38,36 @@ app.MapGet("/house/{houseId:int}", async (int houseId, IHouseRepository repo) =>
     return Results.Ok(house);
 }).ProducesProblem(404).Produces<HouseDetailDto>(StatusCodes.Status200OK);
 
+// Create end-point
+app.MapPost("/houses", async ([FromBody] HouseDetailDto dto, IHouseRepository repo) =>
+{
+    var newHouse = repo.Add(dto);
 
+    return Results.Created($"/house/{newHouse.Id}", newHouse);
+
+}).Produces<HouseDetailDto>(StatusCodes.Status201Created);
+
+// Update end-point
+app.MapPut("/houses", async ([FromBody] HouseDetailDto dto, IHouseRepository repo) =>
+{
+    if (await repo.Get(dto.Id) == null)
+        return Results.Problem($"House {dto.Id} not found", statusCode: 404);
+
+    var updatedHouse = await repo.Update(dto);
+
+    return Results.Ok(updatedHouse);
+
+}).ProducesProblem(404).Produces<HouseDetailDto>(StatusCodes.Status200OK);
+
+// Delete end-point
+app.MapDelete("/houses/{houseId:int}", async (int houseId, IHouseRepository repo) =>
+{
+    if (await repo.Get(houseId) == null)
+        return Results.Problem($"House {houseId} not found", statusCode: 404);
+
+    await repo.Delete(houseId);
+
+    return Results.Ok();
+}).ProducesProblem(404).Produces(StatusCodes.Status200OK);
 
 app.Run();
