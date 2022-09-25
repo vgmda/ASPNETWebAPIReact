1c605ffc7a54296a01ff4e8001aaa4d03885a05a
vgmda <vasilegancin@gmail.com>
2022-09-25 23:51:26 +0100

    Implement HouseAdd()

--- src/house/HouseAdd.tsx
+++ src/house/HouseAdd.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import useFetchHouses, { useFetchHouse } from '../hooks/HouseHooks';
+import useFetchHouses, { useAddHouse, useFetchHouse } from '../hooks/HouseHooks';
 import { currencyFormatter } from '../config';
 import ApiStatus from '../ApiStatus';
 import { useParams } from 'react-router-dom';
 import defaultPhoto from './defaultPhoto';
+import { House } from '../types/houses';
+import HouseForm from './HouseForm';
 
 const HouseAdd = () => {
-    
+    const addHouseMutation = useAddHouse();
+
+    const house: House = {
+        address: "",
+        country: "",
+        description: "",
+        price: 0,
+        id: 0,
+        photo: ""
+    };
+
+    return (
+        <HouseForm
+            house={house}
+            submitted={(h) => addHouseMutation.mutate(h)}
+        />
+    );
 }
 
 export default HouseAdd;
