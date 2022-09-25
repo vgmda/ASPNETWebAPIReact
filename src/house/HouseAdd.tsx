245e8616ff8f3ecf4304f94803584f13bc9c8418
vgmda <vasilegancin@gmail.com>
2022-09-25 23:35:30 +0100

    Add HouseAdd() file

---
+++ src/house/HouseAdd.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import useFetchHouses, { useFetchHouse } from '../hooks/HouseHooks';
+import { currencyFormatter } from '../config';
+import ApiStatus from '../ApiStatus';
+import { useParams } from 'react-router-dom';
+import defaultPhoto from './defaultPhoto';
+
+const HouseAdd = () => {
+    
+}
+
+export default HouseAdd;
