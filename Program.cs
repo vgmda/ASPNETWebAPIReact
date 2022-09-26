0ad2262c71cb24437c84458377639af4f8b1eaab
vgmda <vasilegancin@gmail.com>
2022-09-26 22:50:11 +0100

    Add server-side validation

--- Program.cs
+++ Program.cs
@@ -41,30 +41,39 @@ app.MapGet("/house/{houseId:int}", async (int houseId, IHouseRepository repo) =>
 // Create end-point
 app.MapPost("/houses", async ([FromBody] HouseDetailDto dto, IHouseRepository repo) =>
 {
+    if (!MiniValidator.TryValidate(dto, out var errors))
+    {
+        return Results.ValidationProblem(errors);
+    }
     var newHouse = await repo.Add(dto);
-
     return Results.Created($"/house/{newHouse.Id}", newHouse);
 
-}).Produces<HouseDetailDto>(StatusCodes.Status201Created);
+}).Produces<HouseDetailDto>(StatusCodes.Status201Created).ProducesValidationProblem();
 
 // Update end-point
 app.MapPut("/houses", async ([FromBody] HouseDetailDto dto, IHouseRepository repo) =>
 {
+    if (!MiniValidator.TryValidate(dto, out var errors))
+    {
+        return Results.ValidationProblem(errors);
+    }
     if (await repo.Get(dto.Id) == null)
+    {
         return Results.Problem($"House {dto.Id} not found", statusCode: 404);
-
+    }
     var updatedHouse = await repo.Update(dto);
 
     return Results.Ok(updatedHouse);
 
-}).ProducesProblem(404).Produces<HouseDetailDto>(StatusCodes.Status200OK);
+}).ProducesProblem(404).Produces<HouseDetailDto>(StatusCodes.Status200OK).ProducesValidationProblem();
 
 // Delete end-point
 app.MapDelete("/houses/{houseId:int}", async (int houseId, IHouseRepository repo) =>
 {
     if (await repo.Get(houseId) == null)
+    {
         return Results.Problem($"House {houseId} not found", statusCode: 404);
-
+    }
     await repo.Delete(houseId);
 
     return Results.Ok();
