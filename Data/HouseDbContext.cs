93d79c6e1c80557cfedef64743e8e22f6e8e5194
vgmda <vasilegancin@gmail.com>
2022-09-28 21:39:31 +0100

    Add migration for bids

--- Data/HouseDbContext.cs
+++ Data/HouseDbContext.cs
@@ -2,7 +2,7 @@ public class HouseDbContext : DbContext
 {
     public HouseDbContext(DbContextOptions<HouseDbContext> options) : base(options) { }
     public DbSet<HouseEntity> Houses => Set<HouseEntity>();
-
+    public DbSet<BidEntity> Bids => Set<BidEntity>();
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
     {
