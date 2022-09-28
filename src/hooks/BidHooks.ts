799f3f76e97602160c887fd15db1cec854b62997
vgmda <vasilegancin@gmail.com>
2022-09-28 23:20:12 +0100

    Implement useAddBid()

--- src/hooks/BidHooks.ts
+++ src/hooks/BidHooks.ts
@@ -1,5 +1,5 @@
-import axios, { AxiosError } from "axios";
-import { useQuery } from "react-query";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 import config from "../config";
 import { Bid } from "../types/bid";
 import Problem from "../types/problem";
@@ -11,3 +11,17 @@ const useFetchBids = (houseId: number) => {
       .then((resp) => resp.data)
   );
 };
+
+const useAddBid = () => {
+  const queryClient = useQueryClient();
+  return useMutation<AxiosResponse, AxiosError<Problem>, Bid>(
+    (b) => axios.post(`${config.baseApiUrl}/house/${b.houseId}/bids`, b),
+    {
+      onSuccess: (_, bid) => {
+        queryClient.invalidateQueries(["bids", bid.houseId]);
+      },
+    }
+  );
+};
+
+export { useFetchBids, useAddBid };
