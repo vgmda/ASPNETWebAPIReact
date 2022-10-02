72e41ec0f142190b1d87dd08e96ca5c8336d2aa4
vgmda <vasilegancin@gmail.com>
2022-10-02 20:18:28 +0100

    Logo now redirects to home

--- src/main/Header.tsx
+++ src/main/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import logo from "./GloboLogo.png";
 
 // Header file
@@ -7,16 +8,23 @@ type Args = {
   subtitle: string;
 };
 
+
 const Header = ({ subtitle }: Args) => {
+
+  let navigate = useNavigate();
+  function handleClick() {
+    navigate('/')
+  }
 
   return (
     <header className="row mb-4">
       <div className="col-5">
-        <img src={logo} className="logo" alt="logo" />
+        <img src={logo} onClick={handleClick} className="logo" alt="logo" />
       </div>
       <div className="col-7 mt-5 subtitle">{subtitle}</div>
     </header>
   );
 };
+
 
 export default Header;
